1d7da907e8e36d0e14dac52fbf8f54292ff9c429
Lehman Black <WileESpaghetti@users.noreply.github.com>
2015-12-07 21:37:55 -0600

    Fixes #26 Added plugin data to the package.json and added grunt task to add it to the plugin file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,47 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    pluginHeader: [
+      '',
+      'Plugin Name: Dropbox Upload Widget',
+      'Plugin URI:  <%= pkg.homepage %>',
+      'Description: <%= pkg.description %>',
+      'Version:     <%= pkg.version %>',
+      'Author:      <%= pkg.author.name %>',
+      'Author URI:  <%= pkg.author.url %>',
+      'License:     <%= pkg.license.type %>',
+      'License URI: <%= pkg.license.url %>',
+      'Domain Path: /languages',
+      'Text Domain: <%= pkg.name %>',
+      ''
+    ].join('\n'),
+
+    copy: {
+      build: {
+        src:  ['<%= pkg.name  %>.php'],
+        dest: 'build/'
+      }
+    },
+
+    usebanner: {
+      plugin: {
+        options: {
+          position: 'replace',
+          banner: '<%= pluginHeader %>',
+          replace: ' {{PluginHeader}} ',
+          linebreak: false
+        },
+        files: {
+          src: [ 'build/<%= pkg.name %>.php']
+        }
+      }
+    }
+
   });
 
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-banner');
+
+  grunt.registerTask('build', ['copy:build', 'usebanner:plugin']);
+  grunt.registerTask('default', ['build']);
 };
