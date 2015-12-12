bcaad1afea9c39283809c450f586ca29786109e3
Lehman Black <WileESpaghetti@users.noreply.github.com>
2015-12-12 17:57:44 -0600

    Handle activation, deactivation, and uninstall

--- dropbox-upload-widget.php
+++ dropbox-upload-widget.php
@@ -6,3 +6,8 @@ defined( 'ABSPATH' ) or exit( 'WordPress must be running to use plugin' );
 define( 'DUW_PLUGIN', true );
 
 require_once( 'inc/class-duw-plugin.php' );
+
+register_activation_hook( __FILE__, array( 'DUW_Plugin', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'DUW_Plugin', 'deactivate' ) );
+register_uninstall_hook( __FILE__, array( 'DUW_Plugin', 'uninstall' ) );
+
--- inc/class-duw-plugin.php
+++ inc/class-duw-plugin.php
@@ -6,4 +6,24 @@
  * Contains global plugin metadata and plugin management functions
  */
 class DUW_Plugin {
+	/**
+	 * Plugin activation hook for {{PLUGIN_NAME}}
+	 */
+	public static function activate() {
+		// FIXME stub
+	}
+
+	/**
+	 * Plugin deactivation hook for {{PLUGIN_NAME}}
+	 */
+	public static function deactivate() {
+		// FIXME stub
+	}
+
+	/**
+	 * Plugin deactivation hook for {{PLUGIN_NAME}}
+	 */
+	public static function uninstall() {
+		// FIXME stub
+	}
 }
