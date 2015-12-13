b90c96e44418c1b9dc6d66c9ccc7512174cd4031
Lehman Black <WileESpaghetti@users.noreply.github.com>
2015-12-12 22:50:35 -0600

    Add role to represent guests and remove on uninstall

--- inc/class-duw-permissions.php
+++ inc/class-duw-permissions.php
@@ -1,10 +1,26 @@
 <?php
 
 class DUW_Permissions {
+
+	/**
+	 * @var array meta information for the custom guest role
+	 */
+	public static $GUEST_ROLE = array(
+			'slug' => 'duw_guest',
+			'name' => 'Guest'
+	);
+
 	/**
 	 * Initialize the plugin's permissions if needed
 	 */
 	public static function init() {
+		if (! DUW_Plugin::inHook('activate')) {
+			return;
+		}
+
+		// Add custom guest role if needed
+		$guestAdded = add_role(static::$GUEST_ROLE['slug'], static::$GUEST_ROLE['name'], array());
+
 		// FIXME stub
 	}
 
@@ -19,6 +35,13 @@ class DUW_Permissions {
 	 * Remove any custom roles and capabilities
 	 */
 	public static function remove() {
+		$allowRemove = ( DUW_Plugin::inHook('uninstall') || current_user_can('edit_users') );
+		if (! $allowRemove ) {
+			return;
+		}
+
+		remove_role( static::$GUEST_ROLE['slug'] );
+
 		// FIXME stub
 	}
 }
