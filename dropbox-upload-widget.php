c9c7f4d85f5b489f20ec01f09fef5c0596d5d157
Lehman Black <WileESpaghetti@users.noreply.github.com>
2015-12-14 22:11:57 -0600

    Add link to plugin settings page

--- dropbox-upload-widget.php
+++ dropbox-upload-widget.php
@@ -14,3 +14,5 @@ register_deactivation_hook( __FILE__, array( 'DUW_Plugin', 'deactivate' ) );
 register_uninstall_hook( __FILE__, array( 'DUW_Plugin', 'uninstall' ) );
 
 add_filter( 'editable_roles', array( 'DUW_Permissions', 'hide_guest_role' ), 11, 1 );
+
+add_action( 'admin_menu', array( 'DUW_Settings', 'the_menu_item' ) );
--- inc/class-duw-settings.php
+++ inc/class-duw-settings.php
@@ -6,5 +6,35 @@
  * {{PLUGIN_NAME}} settings management functions
  */
 class DUW_Settings {
+	const OPTION_NAME = '';
+	const OPTIONS_PAGE_TITLE = DUW_Plugin::NAME . 'Settings';
+	const OPTIONS_PAGE_PERMS = 'manage_options';
+	const OPTIONS_MENU_TITLE = DUW_PLUGIN::NAME;
+	const OPTIONS_MENU_SLUG = DUW_PLUGIN;
 
+	/**
+	 * Action to add link to the plugin settings page to the Settings menu.
+	 *
+	 * This needs to be be run from the `admin_menu` hook
+	 */
+	public static function the_menu_item() {
+		if ( current_filter() !== 'admin_menu' ) {
+			return;
+		}
+
+		add_options_page(
+			static::OPTIONS_PAGE_TITLE,
+			static::OPTIONS_MENU_TITLE,
+			static::OPTIONS_PAGE_PERMS,
+			static::OPTIONS_MENU_SLUG,
+			array( get_called_class(), 'the_options_page' )
+		);
+	}
+
+	/**
+	 * Callback to render the settings page for {{PLUGIN_NAME}}
+	 */
+	public static function the_options_page() {
+		// FIXME stub
+	}
 }
