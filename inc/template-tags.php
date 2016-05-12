37b37349afc41f6ff120ce575ecbf70e5822739c
Lehman Black <WileESpaghetti@users.noreply.github.com>
2016-05-11 21:56:18 -0500

    Add template tag: email

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -43,3 +43,39 @@ function duw_the_display_name() {
      */
     echo apply_filters('duw_the_display_name', $displayName, DUW_Dropbox::account_info());
 }
+
+/**
+ * Retrieve the email address associated with the configured Dropbox account.
+ *
+ * @since 0.0.0
+ *
+ * @return string|false The email address or false if there was an error getting the Dropbox account information
+ */
+function duw_get_the_email() {
+    $accountInfo = DUW_Dropbox::account_info();
+
+    if (empty($accountInfo) || empty($accountInfo['email'])) {
+        return false;
+    }
+
+    return $accountInfo['email'];
+}
+
+/**
+ * Display the email address associated with the configured Dropbox account.
+ *
+ * @since 0.0.0
+ */
+function duw_the_email() {
+    $email = duw_get_the_email();
+
+    /**
+     * Filter the Dropbox email address.
+     *
+     * @since 0.0.0
+     *
+     * @param string $email          The Dropbox user's email address.
+     * @param array  $accountInfo    The raw Dropbox account information pulled from the API.
+     */
+    echo apply_filters('duw_the_email', $email, Duw_Dropbox::account_info());
+}
