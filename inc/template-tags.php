a78cc9b7b1b8bb12f206cc79681487d5d60b85c4
Lehman Black <WileESpaghetti@users.noreply.github.com>
2016-05-11 21:45:45 -0500

    Add template tag: display name

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -8,3 +8,38 @@
  * @subpackage Template
  */
 
+/**
+ * Retrieve the display name of the configured Dropbox account.
+ *
+ * @since 0.0.0
+ *
+ * @return string|false The display name or false if there was an error getting the Dropbox account information
+ */
+function duw_get_the_display_name() {
+    $accountInfo = DUW_Dropbox::account_info();
+
+    if (empty($accountInfo) || empty($accountInfo['display_name'])) {
+        return false;
+    }
+
+    return $accountInfo['display_name'];
+}
+
+/**
+ * Display the display name of the configured Dropbox account.
+ *
+ * @since 0.0.0
+ */
+function duw_the_display_name() {
+    $displayName = duw_get_the_display_name();
+
+    /**
+     * Filter the display name of the configured Dropbox account.
+     *
+     * @since 0.0.0
+     *
+     * @param string $usage        The Dropbox usage.
+     * @param array  $quotaInfo    The raw quota information from the Dropbox account information.
+     */
+    echo apply_filters('duw_the_display_name', $displayName, DUW_Dropbox::account_info());
+}
